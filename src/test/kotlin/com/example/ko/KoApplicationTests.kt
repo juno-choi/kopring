7e9f31a1e7d9333d8fe4ecd9692193eea9cbf4ae
juno <juno.c@kakaovx.com>
2022-08-29 15:26:00 +0900

    first commit

---
+++ src/test/kotlin/com/example/ko/KoApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.ko
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KoApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
